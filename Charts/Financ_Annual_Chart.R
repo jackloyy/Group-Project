1b787c56d32767f024b138091fd372edea4d62ec
Jack Lo <Jack@D-10-157-160-163.dhcp4.washington.edu>
2017-05-26 00:06:58 -0700

    Added Financial Chart

--- Charts/Financ_Annual_Chart.R
+++ Charts/Financ_Annual_Chart.R
@@ -1,38 +1,41 @@
-library(dplyr)
-
-tourism.data <- read.csv('../Data/JTM_inbound20170421eng.csv', stringsAsFactors=FALSE)
-
-# Select date(X) and country columns(X.11)
-usa.data <- tourism.data %>% select(X, X.11)
-
-# Creates dataset of monthly visits
-usa.monthly <- usa.data %>% filter(row_number() >= 5, row_number() <= 256) %>% mutate(Years = as.integer(1996 + ((row_number() - 1) / 12)))
-colnames(usa.monthly) <- c("Months", "Total", "Years")
-# Removes years from Months column
-i <- 1
-while(i < nrow(usa.monthly)) {
-  usa.monthly$Months[i] = "Jan．"
-  i = i + 12;
-}
-# Change value to match up with the rest from the same column
-usa.monthly$Months[46] = "Oct．"
-# Creates dataset of annual visits (1996 - 2016)
-usa.annual <- usa.data %>% filter(row_number() >= 266, row_number() <= 292)
-colnames(usa.annual) <- c("Years", "Total")
-
-currency <- read.csv('../Data/currency_rates.csv', stringsAsFactors=FALSE);
-income <- read.csv('../Data/income.csv', stringsAsFactors=FALSE);
-
-ay <- list(
-  tickfont = list(color = "red"),
-  overlaying = "y",
-  side = "right",
-  title = "Currency" # Finance
-)
-p <- plot_ly() %>%
-  add_lines(x = ~usa.annual$Years, y = ~usa.annual$Total, name = "Visits v.s Year") %>%
-  add_lines(x = ~usa.annual$Years, y = ~1:3, name = "Currency v.s Year", yaxis = "y2") %>%
-  layout(
-    title = "Financial Impact on U.S Visits to Japan", yaxis2 = ay,
-    xaxis = list(title="x")
+# Displays a multiple axes chart given data.annual (dataset) and the type of financial information, typeFinance
+financial.annual.chart <- function(data.annual, typeFinance) {
+  # Reads in the financial datasets
+  currency <- read.csv('../Data/currency_rates.csv', stringsAsFactors=FALSE) %>% filter(Years != "2016")
+  income <- read.csv('../Data/income.csv', stringsAsFactors=FALSE)
+  # Filters our the year 2016 to match the financial datasets
+  data.annual <- data.annual %>% filter(Years != "2016")
+  
+  # Stores and chops off the commas in the numbers
+  usa.total <- as.numeric(gsub(",", "", data.annual$Total))
+  currency.total <- as.numeric(gsub(",", "", currency$Total))
+  income.total <- as.numeric(gsub(",", "", income$Total))
+  
+  financial.total <- ""
+  financial.type <- ""
+  # Initializes the type of financial information specified
+  if(typeFinance == "currency") {
+    financial.total <- currency.total
+    financial.type <- "Currency"
+  } else if(typeFinance == "income") {
+    financial.total <- income.total
+    financial.type <- "Income"
+  }
+  
+  ay <- list(
+    tickfont = list(color = "red"),
+    overlaying = "y",
+    side = "right",
+    title = financial.type
   )
+  
+  p <- plot_ly() %>%
+    add_lines(x = ~data.annual$Years, y = ~usa.total, name = "Visits v.s Year") %>%
+    add_lines(x = ~data.annual$Years, y = ~financial.total, name = paste(financial.type, "v.s Year"), yaxis = "y2") %>%
+    layout(
+      title = "Financial Impact on U.S Visits to Japan", yaxis2 = ay,
+      xaxis = list(title="Years"),
+      yaxis = list(title="Total Visitors (In Million)")
+    )
+  return (p)
+}
